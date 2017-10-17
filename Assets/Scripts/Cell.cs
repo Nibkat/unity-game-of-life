bb813f347b36948a030ff25814c3d383b0199f8f
Koen Sparreboom <koensparreboom@gmail.com>
2017-10-17 10:01:08 +0200

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private SpriteRenderer spr;
+
+    public Vector2 cellPosition = Vector2.zero;
+
+    private bool alive;
+    public bool tempAlive;
+
+    private void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        SetAlive(!alive);
+
+        print("Alive neighbors: " + CountNeighbors);
+    }
+
+    public void SetAlive(bool value)
+    {
+        alive = value;
+
+        if (alive)
+        {
+            spr.color = Color.black;
+        }
+        else
+        {
+            spr.color = Color.white;
+        }
+    }
+
+    public bool Alive
+    {
+        get
+        {
+            return alive;
+        }
+    }
+
+    public int CountNeighbors
+    {
+        get
+        {
+            int neighbors = 0;
+
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    try
+                    {
+                        if (Grid.instance.cellArray[(int)cellPosition.x + x, (int)cellPosition.y + y].GetComponent<Cell>().alive && Grid.instance.cellArray[(int)cellPosition.x + x, (int)cellPosition.y + y].GetComponent<Cell>() != this)
+                        {
+                            neighbors++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+
+            return neighbors;
+        }
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour
+{
+    public static Grid instance;
+
+    [Header("Grid")]
+    [SerializeField]
+    private GameObject cell;
+
+    [SerializeField]
+    private Vector2 gridSize = new Vector2(4, 4);
+    public GameObject[,] cellArray;
+
+    private GameObject grid;
+
+    [Header("Simulation")]
+    [SerializeField]
+    private bool simulate = false;
+    [SerializeField]
+    private float simulationSpeed = 1f;
+    [SerializeField]
+    private Slider simulationSpeedSlider;
+    [SerializeField]
+    private int generations;
+    [SerializeField]
+    private Text generationText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        GenerateGrid();
+        StartCoroutine(Simulate());
+    }
+
+    public void GenerateGrid()
+    {
+        if (grid != null)
+        {
+            Destroy(grid);
+        }
+
+        Transform gridParent = new GameObject("Grid Parent").transform;
+
+        Vector2 cellSize = new Vector2(cell.GetComponent<SpriteRenderer>().bounds.size.x, cell.GetComponent<SpriteRenderer>().bounds.size.y);
+
+        cellArray = new GameObject[(int)gridSize.x, (int)gridSize.y];
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                cellArray[x, y] = Instantiate(cell, new Vector3(-(cellSize.x * gridSize.x / 2) + cellSize.x * x, -(cellSize.y * gridSize.y / 2) + cellSize.y * y, 0), Quaternion.identity);
+                cellArray[x, y].transform.SetParent(gridParent);
+                
+                cellArray[x, y].GetComponent<Cell>().cellPosition = new Vector2(x, y);
+
+                cellArray[x, y].name = "Cell " + x + " - " + y;
+            }
+        }
+
+        grid = gridParent.gameObject;
+
+        generations = 0;
+        generationText.text = "Generation: " + generations + "\n------------------------------";
+    }
+
+    public void NextGeneration()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                if (cellArray[x, y].GetComponent<Cell>().Alive)
+                {
+                    if (cellArray[x, y].GetComponent<Cell>().CountNeighbors == 3 || cellArray[x, y].GetComponent<Cell>().CountNeighbors == 2)
+                    {
+                        cellArray[x, y].GetComponent<Cell>().tempAlive = true;
+                    }
+                    else
+                    {
+                        cellArray[x, y].GetComponent<Cell>().tempAlive = false;
+                    }
+                }
+                else
+                {
+                    if (cellArray[x, y].GetComponent<Cell>().CountNeighbors == 3)
+                    {
+                        cellArray[x, y].GetComponent<Cell>().tempAlive = true;
+                    }
+                    else
+                    {
+                        cellArray[x, y].GetComponent<Cell>().tempAlive = false;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                cellArray[x, y].GetComponent<Cell>().SetAlive(cellArray[x, y].GetComponent<Cell>().tempAlive);
+            }
+        }
+
+        generations++;
+        generationText.text = "Generation: " + generations + "\n------------------------------";
+    }
+
+    private IEnumerator Simulate()
+    {
+        while (true)
+        {
+            if (simulate)
+            {
+                NextGeneration();
+            }
+
+            yield return new WaitForSeconds(simulationSpeed);
+        }
+    }
+
+    public void RandomizeGrid()
+    {
+        generations = 0;
+        generationText.text = "Generation: " + generations + "\n------------------------------";
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                cellArray[x, y].GetComponent<Cell>().SetAlive(Random.Range(0, 2) == 0);
+            }
+        }
+    }
+
+    public void SetSimulationSpeed()
+    {
+        simulationSpeed = simulationSpeedSlider.value;
+
+        Debug.Log("Simulation speed: " + simulationSpeed);
+    }
+
+    public void ToggleSimulate()
+    {
+        simulate = !simulate;
+    }
+}
